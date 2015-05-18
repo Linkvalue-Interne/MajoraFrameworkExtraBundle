57a18f41d44c9ddbe8f9ca85c9a3693c0889acfe
Quentin Cerny <qc@kptivestudio.com>
2015-05-18 22:53:36 +0200

    Quick fix

---
+++ src/Majora/Bundle/FrameworkExtraBundle/Controller/AdminControllerTrait.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Majora\Bundle\FrameworkExtraBundle\Controller;
+
+use Majora\Bundle\FrameworkExtraBundle\Controller\ControllerTrait;
+
+trait AdminControllerTrait
+{
+    use ControllerTrait;
+}
---
+++ src/Majora/Bundle/FrameworkExtraBundle/Controller/ControllerTrait.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Majora\Bundle\FrameworkExtraBundle\Controller;
+
+trait ControllerTrait
+{
+
+}
