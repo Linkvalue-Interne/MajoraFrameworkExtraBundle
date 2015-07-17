5edbfddba640b735e06610f46f986561e2c21ac0
Quentin Cerny <quentin.cerny@gmail.com>
2015-07-17 16:34:49 +0200

    Externalize security and form bridges from loader trait

---
+++ src/Majora/Framework/Loader/Bridge/Form/DataTransformerLoaderTrait.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Majora\Framework\Loader\Bridge\Form;
+
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+/**
+ * Trait which provides a bridge between majora loader and symfony forms
+ *
+ * @see DataTransformerInterface
+ *
+ * @property entityClass
+ * @method retrieve($id) : CollectionableInterface
+ */
+trait DataTransformerLoaderTrait
+{
+    /**
+     * Model -> View
+     *
+     * @see DataTransformerInterface::transform()
+     */
+    public function transform($entity)
+    {
+        if (null === $entity) {
+            return '';
+        }
+        if (get_class($entity) != $this->entityClass) {
+            throw new \InvalidArgumentException(sprintf(
+                'Unsupported entity "%s" into "%s" loader.',
+                get_class($entity),
+                __CLASS__
+            ));
+        }
+
+        return $entity->getId();
+    }
+
+    /**
+     * View -> Model
+     *
+     * @see DataTransformerInterface::reverseTransform()
+     */
+    public function reverseTransform($id)
+    {
+        if (!$id) {
+            return null;
+        }
+        if (!$entity = $this->retrieve($id)) {
+            throw new TransformationFailedException(sprintf(
+                '%s#%s cannot be found.',
+                $this->entityClass,
+                $id
+            ));
+        }
+
+        return $entity;
+    }
+}
